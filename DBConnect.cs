1ca767ef9a6f5af34c56fa45f52caa807df4433b
yiuscode <48811272+yiuscode@users.noreply.github.com>
2019-07-19 18:25:59 +1000

    Add files via upload

---
+++ DBConnect.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApp1
+{
+    class DBConnect
+    {
+        private MySqlConnection _connection;
+        private string dbHost;
+        private string dbUser;
+        private string dbPass;
+        private string dbName;
+
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            dbHost = "127.0.0.1";
+            dbUser = "root";
+            dbPass = "123456";
+            dbName = "MYDB";
+
+            string connStr = "server=" + dbHost + ";uid=" + dbUser + ";pwd=" + dbPass + ";database=" + dbName;
+
+            _connection = new MySqlConnection(connStr);
+        }
+
+        public bool OpenConnection()
+        {
+            try
+            {
+                _connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                _connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        public void Insert(string id, string name, string age)
+        {
+            string query = $"INSERT INTO student (id, name, age) VALUES({id},'{name}', {age})";
+
+            if(this.OpenConnection())
+            {
+                MySqlCommand cmd = new MySqlCommand(query, _connection);
+
+                cmd.ExecuteNonQuery();
+
+                this.CloseConnection();
+            }
+
+        }
+
+        public void Delete (string id)
+        {
+            
+            string query = $"Delete from student where id = {id};";
+
+            if (this.OpenConnection())
+            {
+                MySqlCommand cmd = new MySqlCommand(query, _connection);
+
+                cmd.ExecuteNonQuery();
+
+                this.CloseConnection();
+            }
+
+        }
+
+        public List<string>[] Select()
+        {
+            string query = "select * from student;";
+
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+            list[2] = new List<string>();
+
+
+
+            if (this.OpenConnection())
+            {
+                MySqlCommand cmd = new MySqlCommand(query, _connection);
+
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+
+                {
+                    list[0].Add(dataReader["id"] + "");
+                    list[1].Add(dataReader["name"] + "");
+                    list[2].Add(dataReader["age"] + "");
+                }
+
+                dataReader.Close();
+
+                this.CloseConnection();
+
+                return (list);
+
+
+            }
+            else
+            {
+                return (list);
+            }
+
+        }
+
+
+
+
+
+
+    }
+
+
+}
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private DBConnect _dbconnect;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _dbconnect = new DBConnect();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            RefreshList();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+
+            _dbconnect.Insert(textBox1.Text, textBox2.Text, textBox3.Text);
+            RefreshList();
+
+        }
+
+        private void RefreshList()
+        {
+            List<string>[] list;
+            list = _dbconnect.Select();
+            dataGridView1.Rows.Clear();
+            for (int i = 0; i < list[0].Count; i++)
+            {
+                int number = dataGridView1.Rows.Add();
+                dataGridView1.Rows[number].Cells[0].Value = list[0][i];
+                dataGridView1.Rows[number].Cells[1].Value = list[1][i];
+                dataGridView1.Rows[number].Cells[2].Value = list[2][i];
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            _dbconnect.Delete(dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            RefreshList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
